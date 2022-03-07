bc5428d697fb501ae2551af69cde53f3fbd42ffe
Henry Gelderbloem <hgelderbloem@icloud.com>
2022-03-07 12:22:02 +0000

    Cleaned up script

--- 2_Building_Images_AVD/0_installFsLogix.ps1
+++ 2_Building_Images_AVD/0_installFsLogix.ps1
@@ -1,28 +1,37 @@
+# Setup of the required environment variables
+$appName = 'FsLogix'
+$tempDirectory = 'C:\Temp\'
+New-Item `
+    -Path $tempDirectory `
+    -Name $appName `
+    -ItemType Directory `
+    -ErrorAction SilentlyContinue
+$LocalPath = $tempDirectory + $appName
+
+# Download FsLogix
 Write-Host 'AIB Customization: Downloading FsLogix'
 $fsLogixURL="https://aka.ms/fslogix_download"
 $installerFile="fslogix_download.zip"
-New-Item `
-    -Path C:\Temp `
-    -Name fslogix `
-    -ItemType Directory `
-    -ErrorAction SilentlyContinue
-$LocalPath = 'C:\Temp\fslogix'
-(New-Object System.Net.WebClient).DownloadFile("$fsLogixURL","$LocalPath\$installerFile")
+$outputPath = $LocalPath + '\' + $installerFile
+(New-Object System.Net.WebClient).DownloadFile("$fsLogixURL","$outputPath")
 Expand-Archive `
-    -Path $LocalPath\$installerFile `
+    -Path $outputPath `
     -DestinationPath $LocalPath `
+    -Force `
     -Verbose
 Write-Host 'AIB Customization: Downloading of FsLogix installer finished'
 
+# Comparing FsLogix Version
 Write-Host 'AIB Customization: Comparing FsLogix versions'
+
 $downloadedFsLogixVersion = Get-Item $LocalPath\x64\Release\FSLogixAppsSetup.exe | Select-Object VersionInfo
-Write-Host 'AIB Customization: Downloaded version number:' $downloadedFsLogixVersion.VersionInfo.FileVersion
+Write-Host 'AIB Customization: Downloaded FsLogix version number:' $downloadedFsLogixVersion.VersionInfo.FileVersion
 
 $installedFsLogixVersion = Get-Item "C:\Program Files\FSLogix\Apps\frx.exe" | Select-Object VersionInfo
-Write-Host 'AIB Customization: Installed version number:' $installedFSLogixVersion.VersionInfo.FileVersion
+Write-Host 'AIB Customization: Installed FsLogix version number:' $installedFSLogixVersion.VersionInfo.FileVersion
 
 if ([version]$downloadedFsLogixVersion.VersionInfo.FileVersion -gt [version]$installedFsLogixVersion.VersionInfo.FileVersion) {
-    Write-Host 'AIB Customization: Downloaded version is greator than that installed. Updating FsLogix.'
+    Write-Host 'AIB Customization: Downloaded FsLogix version is greator than that installed. Updating FsLogix.'
     Write-Host 'AIB Customization: Uninstalling FsLogix'
     Start-Process `
     -FilePath $LocalPath\x64\Release\FSLogixAppsSetup.exe `
@@ -33,14 +42,13 @@ if ([version]$downloadedFsLogixVersion.VersionInfo.FileVersion -gt [version]$ins
     Write-Host 'AIB Customization: Starting Fslogix installer'
     Start-Process `
     -FilePath $LocalPath\x64\Release\FSLogixAppsSetup.exe `
-    -ArgumentList "/install /quiet" `
+    -ArgumentList "/install /quiet /norestart" `
     -Wait `
     -Passthru
+
+    $installedFsLogixVersion = Get-Item "C:\Program Files\FSLogix\Apps\frx.exe" | Select-Object VersionInfo
+    Write-Host 'AIB Customization: Installed FsLogix version number is now:' $installedFSLogixVersion.VersionInfo.FileVersion
+    Write-Host 'AIB Customization: Finished Fslogix installer'
 } else {
-    Write-Host 'AIB Customization: Installed version matches the downloaded version. Skipping FsLogix update.'
+    Write-Host 'AIB Customization: Installed FsLogix version matches the downloaded version. Skipping FsLogix update.'
 }
-
-$installedFsLogixVersion = Get-Item "C:\Program Files\FSLogix\Apps\frx.exe" | Select-Object VersionInfo
-Write-Host 'AIB Customization: Installed version number is now:' $installedFSLogixVersion.VersionInfo.FileVersion
-
-Write-Host 'AIB Customization: Finished Fslogix installer'
