090588e78b458017bcb1003c8d3351ea4af6fe4d
Henry Gelderbloem <hgelderbloem@icloud.com>
2022-04-01 13:00:57 +0100

    Initital version. Still work to do as it errors when comparing teams versions.

--- 2_Building_Images_AVD/1_installTeams.ps1
+++ 2_Building_Images_AVD/1_installTeams.ps1
@@ -1,37 +1,51 @@
-# Setup of the required environment variables
+# Global Variables
+$lastError = @()
+
+# Teams Directory Creation Parameters & Variables
 $appName = 'Teams'
 $tempDirectory = 'C:\Temp\'
-New-Item `
-    -Path $tempDirectory `
-    -Name $appName `
-    -ItemType Directory `
-    -ErrorAction SilentlyContinue
-$LocalPath = $tempDirectory + $appName
+$appPath = $tempDirectory + $appName
+$NewItemParameters = @{
+    Path = $tempDirectory
+    Name = $appName
+    ItemType = "Directory"
+    ErrorAction = "Stop"
+    ErrorVariable = "lastError"
+    Force = $true
+}
 
-# Update Microsoft Teams WebSocket Service
-Write-Host 'AIB Customisation: Downloading the Microsoft Teams WebSocket Service'
+# Teams WebSocket Download Parameters & Variables
 $webSocketsURL = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWQ1UW'
 $webSocketsInstallerMsi = 'webSocketSvc.msi'
-$outputPath = $LocalPath + '\' + $webSocketsInstallerMsi
-(New-Object System.Net.WebClient).DownloadFile("$webSocketsURL","$outputPath")
-Write-Host 'AIB Customisation: Downloading of the Microsoft Teams WebSocket Service finished'
+$outputPath = $appPath + '\' + $webSocketsInstallerMsi
+$InvokeWebRequestParameters = @{
+    Uri = $webSocketsURL
+    OutFile = $outputPath
+    ErrorAction = "Stop"
+    ErrorVariable = "lastError"
+}
 
-Write-Host 'AIB Customisation: Updating the Microsoft Teams WebSocket Service'
-Start-Process `
-    -FilePath msiexec.exe `
-    -Args "/i $outputPath /quiet /norestart /log webSocket.log" `
-    -Wait
-Write-Host 'AIB Customisation: Finished updating the Microsoft Teams WebSocket Service' 
+# Teams WebSocket Install Parameters & Variables
+$webSocketInstallParameters = @{
+    FilePath = "msiexec.exe"
+    Args = "/i $outputPath /quiet /norestart /log webSocket.log"
+    Wait = $true
+    ErrorAction = "Stop"
+    ErrorVariable = "lastError"
+}
 
-# Update Microsoft Teams
-Write-Host 'AIB Customisation: Downloading Microsoft Teams'
-$teamsURL = 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true'
+# Teams Download Parameters & Variables
+$teamsURL = "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true"
 $teamsMsi = 'teams.msi'
-$outputPath = $LocalPath + '\' + $teamsMsi
-(New-Object System.Net.WebClient).DownloadFile("$teamsURL","$outputPath")
-Write-Host 'AIB Customisation: Downloading of Microsoft Teams installer finished'
+$outputPath = $appPath + '\' + $teamsMsi
+$teamsWebRequestParameters = @{
+    Uri = $teamsURL
+    OutFile = $teamsMsi
+    ErrorAction = "Stop"
+    ErrorVariable = "lastError"
+}
 
-Write-Host 'AIB Customisation: Comparing Microsoft Teams versions'
+#Teams Version Comparison Function
 function Get-MsiVersionNumber {
     param (
         [parameter(Mandatory=$true)]
@@ -51,29 +65,27 @@ function Get-MsiVersionNumber {
         throw "Failed to get MSI file version: {0}." -f $_
     }   
 }
-$downloadedTeamsVersion = Get-MsiVersionNumber -MSIPATH $outputPath
-Write-Host 'AIB Customisation: Downloaded Microsoft Teams version number:' $downloadedTeamsVersion
-$installedTeamsVersion = Get-Item "C:\Program Files (x86)\Teams Installer\Teams.exe" | Select-Object VersionInfo
-Write-Host 'AIB Customisation: Installed Microsoft Teams version number:' $installedTeamsVersion.VersionInfo.FileVersion
 
-if ([version]$downloadedTeamsVersion -gt [version]$installedTeamsVersion.VersionInfo.FileVersion) {
-    Write-Host 'AIB Customisation: Downloaded Microsoft Teams version is greator than that installed. Updating Microsoft Teams.'
-    Start-Process `
-    -FilePath msiexec.exe `
-    -Args "/I $outputPath /quiet /norestart /log teamsUpdate.log ALLUSER=1 ALLUSERS=1" `
-    -Wait
-    $installedTeamsVersion = Get-Item "C:\Program Files\Teams Installer\Teams.exe" | Select-Object VersionInfo
-    Write-Host 'AIB Customisation: Installed Microsoft Teams version number is now:' $installedTeamsVersion.VersionInfo.FileVersion
-    Write-Host 'AIB Customisation: Finished updating Microsoft Teams' 
-    } else {
-    Write-Host 'AIB Customisation: Installed Microsoft Teams version matches the downloaded version. Skipping Teams update.'
+# Teams Update Parameters & Variables
+$teamsUpdateParameters = @{
+    FilePath = "msiexec.exe"
+    Argus = "/I $outputPath /quiet /norestart /log teamsUpdate.log ALLUSER=1 ALLUSERS=1"
+    Wait = $true
+    ErrorAction = "Stop"
+    ErrorVariable = "lastError"
 }
 
-# Confirm registry is set correctly
-Write-Host 'AIB Customisation: Checking if Microsoft Teams media optimsation is set'
-$registryPath = "HKLM:SOFTWARE\Microsoft\Teams"
-$valueName = "IsWVDEnvironment"
-$vauleData = "1"
+# Teams Media Optimisations Paramets & Variables
+$optimisationItemPropertyParameters = @{
+    registryPath = "HKLM:SOFTWARE\Microsoft\Teams"
+    vauleName = "IsWVDEnvironment"
+    vauleData = "1"
+}
+$optimisationItemParameters = @{
+    registryPath = "HKLM:SOFTWARE\Microsoft\Teams"
+}
+
+# Teams Optimsation Check Function
 function Test-RegistryValue {
     param (
         [parameter(Mandatory=$true)]
@@ -90,23 +102,108 @@ function Test-RegistryValue {
         }
 }
 
-if (!(Test-Path $registryPath)) {
-    Write-Host 'AIB Customisation: Microsoft Teams media optimisation not set. Enabling Microsoft Teams media optimsation'
-    New-Item `
-        -Path $registryPath `
-        -Force | Out-Null
-    New-ItemProperty `
-        -Path $registryPath `
-        -Name $valueName `
-        -Value $vauleData `
-        -PropertyType Dword
-} elseif (!(Test-RegistryValue -Path $registryPath -Value $valueName)) {
-    Write-Host 'AIB Customisation: Microsoft Teams media optimisation not set. Enabling Microsoft Teams media optimsation'
-    New-ItemProperty `
-        -Path $registryPath `
-        -Name $valueName `
-        -Value $vauleData `
-        -PropertyType Dword
-} else {
-    Write-Host 'AIB Customisation: Microsoft Teams media optimisation set correctly'
+
+# Teams Directory Creation
+# Creates a new folder in C:\Temp with the name Teams.
+try {
+    Write-Host -Object "AIB Customisation: Creating Teams directory."
+    New-Item @NewItemParameters
+    Write-Host -Object "AIB Customisation: Created Teams directory."
+}
+catch {
+    Write-Host -Object "AIB Customisation Error: Unable to create the directory $appPath."
+    Write-Host -Object $lastError
+    Write-Host -Object "Exit code: $LASTEXITCODE"
+}
+
+
+# Teams WebSocket Download & Install
+# Downloads the Teams WebSocket Service into the $appPath directory then updates the WebSocket Service.
+try {
+    Write-Host -Object "AIB Customisation: Downloading the Microsoft Teams WebSocket Service."
+    Invoke-WebRequest @InvokeWebRequestParameters
+    Write-Host -Object "AIB Customisation: Downloaded the Microsoft Teams WebSocket Service."
+    try {
+        Write-Host -Object "AIB Customisation: Updating the Microsoft Teams WebSocket Service"
+        Start-Process @webSocketInstallParameters
+        Write-Host -Object "AIB Customisation: Updated the Microsoft Teams WebSocket Service"
+    }
+    catch {
+        Write-Host -Object "AIB Customisation Error: Unable to update the Microsoft Teams WebSocket Service."
+        Write-Host -Object $lastError
+        Write-Host -Object "Exit code: $LASTEXITCODE"    
+    }
+}
+catch {
+    Write-Host -Object "AIB Customisation Error: Unable to download the Microsoft Teams WebSocket Service."
+    Write-Host -Object $lastError
+    Write-Host -Object "Exit code: $LASTEXITCODE"
+}
+
+
+# Teams Update
+# Updates the Teams Machine-Wide installer. 
+try {
+    Write-Host "AIB Customisation: Downloading Microsoft Teams."
+    Invoke-WebRequest @teamsWebRequestParameters
+    Write-Host "AIB Customisation: Downloaded Microsoft Teams."
+}
+catch {
+    Write-Host -Object "AIB Customisation Error: Unable to download Teams."
+    Write-Host -Object $lastError
+    Write-Host -Object "Exit code: $LASTEXITCODE"
+}
+
+
+# Teams Version Comparison & Update
+# Compares the downloaded version of Teams with the installed version.
+try {
+    Write-Host -Object "AIB Customisation: Comparing Microsoft Teams versions."
+    $downloadedTeamsVersion = Get-MsiVersionNumber -MSIPATH $outputPath
+    Write-Host -Object "AIB Customisation: Downloaded Microsoft Teams version number: $downloadedTeamsVersion."
+    $installedTeamsVersion = Get-Item "C:\Program Files (x86)\Teams Installer\Teams.exe" | Select-Object VersionInfo
+    Write-Host "AIB Customisation: Installed Microsoft Teams version number: $($installedTeamsVersion.VersionInfo.FileVersion)" 
+    if ([version]$downloadedTeamsVersion -gt [version]$installedTeamsVersion.VersionInfo.FileVersion) {
+        try {
+            Write-Host -Object "AIB Customisation: Downloaded Microsoft Teams version is greater than that installed. Updating Microsoft Teams."
+            Start-Process @teamsUpdateParameters
+            $installedTeamsVersion = Get-Item "C:\Program Files\Teams Installer\Teams.exe" | Select-Object VersionInfo
+            Write-Host -Object "AIB Customisation: Installed Microsoft Teams version number is now: $($installedTeamsVersion.VersionInfo.FileVersion)."
+            Write-Host -Object "AIB Customisation: Updated Microsoft Teams."
+        }
+        catch {
+            Write-Host -Object "AIB Customisation Error: Unable to update Teams."
+            Write-Host -Object $lastError
+            Write-Host -Object "Exit code: $LASTEXITCODE"        
+        }  else {
+            Write-Host -Object "AIB Customisation: Installed Microsoft Teams version matches the downloaded version. Skipping Teams update."
+        }      
+    }
+}
+catch {
+    Write-Host -Object "AIB Customisation Error: Unable to compare Teams version numbers."
+    Write-Host -Object $lastError
+    Write-Host -Object "Exit code: $LASTEXITCODE"        
+}
+
+
+# Teams AVD Optimisation Check
+# Confirms that AVD Optimisations are enabled, if not, they will be enabled.
+try {
+    Write-Host -Object "AIB Customisation: Checking if Microsoft Teams Media optimsations are enabled."
+    if (!(Test-Path $registryPath)) {
+        Write-Host -Object "AIB Customisation: Microsoft Teams media optimisations are not enabled. Enabling Microsoft Teams media optimsations."
+        New-Item @optimisationItemParameters
+        New-ItemProperty @optimisationItemPropertyParameters        
+    } elseif (!(Test-RegistryValue -Path $registryPath -Value $valueName)) {
+        Write-Host -Object "AIB Customisation: Microsoft Teams media optimisations are not enabled. Enabling Microsoft Teams media optimsations."
+        New-ItemProperty @optimisationItemPropertyParameters
+    } else {
+        Write-Host "AIB Customisation: Microsoft Teams media optimisation are now enabled."
+    }
+}
+catch {
+    Write-Host -Object "AIB Customisation Error: Unable to enable Microsoft Teams optimisations."
+    Write-Host -Object $lastError
+    Write-Host -Object "Exit code: $LASTEXITCODE"        
 }
